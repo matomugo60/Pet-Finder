4409ba2cae5f3d07a2ddfd6fcabe20f058c739ab
matomugo60 <matomugo60@gmail.com>
2023-02-27 14:29:13 +0300

    Defined app.js

--- petfinder-client/src/app /App.js
+++ petfinder-client/src/app /App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import './App.css';
+import Login from './Login';
+import Register from './Register';
+import PetList from './PetList';
+import AddPet from './AddPet';
+import EditPet from './EditPet';
+import DeletePet from './DeletePet';
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Petfinder</Link>
+            </li>
+            {user ? (
+              <>
+                <li>
+                  <Link to="/pets">View Your Pets</Link>
+                </li>
+                <li>
+                  <Link to="/pets/new">Add a Pet</Link>
+                </li>
+                <li>
+                  <button onClick={() => setUser(null)}>Logout</button>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <Link to="/login">Login</Link>
+                </li>
+                <li>
+                  <Link to="/register">Register</Link>
+                </li>
+              </>
+            )}
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/login">
+            <Login setUser={setUser} />
+          </Route>
+          <Route path="/register">
+            <Register setUser={setUser} />
+          </Route>
+          <Route exact path="/pets">
+            <PetList user={user} />
+          </Route>
+          <Route path="/pets/new">
+            <AddPet user={user} />
+          </Route>
+          <Route path="/pets/:petId/edit">
+            <EditPet user={user} />
+          </Route>
+          <Route path="/pets/:petId/delete">
+            <DeletePet user={user} />
+          </Route>
+          <Route path="/">
+            <h1>Welcome to Petfinder</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
