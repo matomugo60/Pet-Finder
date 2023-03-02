5d0c628eb678ee4a5f5b7acf1a36b1f12a7ecae8
matomugo60 <matomugo60@gmail.com>
2023-03-02 13:31:12 +0300

    changed register component

--- petfinder-client/src/components/Register.js
+++ petfinder-client/src/components/Register.js
@@ -1,46 +1,106 @@
-import React, { useState } from 'react';
+import React,{useState} from "react";
+import { useNavigate,Link } from "react-router-dom";
 
-function Register({ setUser }) {
-const [name, setName] = useState('');
-const [email, setEmail] = useState('');
-const [password, setPassword] = useState('');
+function Register({setIsLoggedIn}){
+   const [formData, setFormData] = useState({
+    username:"",
+    firstname: "",
+    lastname:"",  
+    email: "",
+    password: "",
+    
 
-const handleSubmit = (e) => {
-e.preventDefault();
-// Send a POST request to backend API to register user
-fetch('/api/register', {
-    method: 'POST',
-    body: JSON.stringify({ name, email, password }),
-    headers: { 'Content-Type': 'application/json' },
-  })
-    .then((response) => response.json())
-    .then((data) => setUser(data.user))
-    .catch((error) => console.error(error));
-};
+  });
+  let nav = useNavigate();
 
-return (
-<div>
-<h1>Register</h1>
-<form onSubmit={handleSubmit}>
-<label>
-Name:
-<input type="text" value={name} onChange={(e) => setName(e.target.value)} />
-</label>
-<br />
-<label>
-Email:
-<input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-</label>
-<br />
-<label>
-Password:
-<input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-</label>
-<br />
-<button type="submit">Submit</button>
-</form>
-</div>
-);
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  }
+  function handleSubmit(e) {
+    e.preventDefault();
+        setIsLoggedIn(true)
+        nav("/");
+      
+    }
+    return (
+        <div className="container">
+          <h1 className="text-center mb-4">Sign Up</h1>
+
+          <form onSubmit={handleSubmit} className="row g-3">
+        <div className="form-group mb-2 col-md-6">
+          <label htmlFor="firstname">First Name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="First Name"
+            name="firstname"
+            id="firstname"
+            onChange={handleChange}
+            value={formData.firstname}
+          />
+        </div>
+        <div className="form-group mb-2 col-md-6">
+          <label htmlFor="lastname">Last Name</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Last Name"
+            name="lastname"
+            id="lastname"
+            onChange={handleChange}
+            value={formData.lastname}
+          />
+        </div>
+        <div className="form-group mb-2 col-6 align-items-center">
+          <label htmlFor="username">Username</label>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Username"
+            name="username"
+            id="username"
+            onChange={handleChange}
+            value={formData.username}
+          />
+        </div>
+        <div className="form-group mb-2 col-md-6">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Email Address"
+            name="email"
+            id="email"
+            onChange={handleChange}
+            value={formData.email}
+          />
+        </div>
+        <div className="form-group mb-4 col-md-6">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Password"
+            name="password"
+            id="password"
+            onChange={handleChange}
+            value={formData.password}
+          />
+        </div>
+        <center><button type="submit" className="btn btn-primary mb-4">Submit</button></center>
+
+        <center>
+        <p className="forgot-password text-right">
+          Already registered <Link to="/">Login?</Link>
+        </p>
+        </center>
+      </form>
+      
+        </div>
+    );
 }
 
-export default Register;  
+export default Register
