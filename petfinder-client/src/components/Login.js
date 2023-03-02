1c00d516eca3cc53a0bcef1531f939a464b31b26
matomugo60 <matomugo60@gmail.com>
2023-03-02 13:30:29 +0300

    changed login component

--- petfinder-client/src/components/Login.js
+++ petfinder-client/src/components/Login.js
@@ -1,40 +1,81 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
+import { useNavigate,Link } from "react-router-dom";
 
-function Login({ setUser }) {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+function Login() {
+ 
+  const [formData, setFormData] = useState({
+    email: "",
+    password: ""
+  });
+  
+  let nav = useNavigate();
 
-  const handleSubmit = (e) => {
+  function handleChange(e) {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  }
+
+  function handleSubmit(e) {
     e.preventDefault();
+        
+        nav("/");
+      
+    }
 
-    // Send a POST request to backend API to authenticate user
-    fetch('/api/login', {
-      method: 'POST',
-      body: JSON.stringify({ email, password }),
-      headers: { 'Content-Type': 'application/json' },
-    })
-      .then((response) => response.json())
-      .then((data) => setUser(data.user))
-      .catch((error) => console.error(error));
-  };
+    const backgroundStyle = {
+      backgroundImage: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsZwg9ojutvztDmCfKCzcEi860Kf64SRPKlw&usqp=CAU",
+      backgroundSize: "cover",
+      backgroundPosition: "center",
+      height: "100vh"
+    };
 
   return (
-    <div>
-      <h1>Login</h1>
-      <form onSubmit={handleSubmit}>
-        <label>
-          Email:
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <div style={backgroundStyle}>
+    <div className="container">
+    <h1 className="text-center mb-4">Login</h1>
+    
+    <form onSubmit={handleSubmit} >
+      <div className="row justify-content-center">
+      <div className="form-group mb-2 col-md-4">
+        <label htmlFor="email">Email</label>
+        <input
+          type="email"
+          placeholder="Email Address"
+          name="email"
+          id="email"
+          className="form-control"
+          onChange={handleChange}
+          value={formData.email}
+        />
+        </div>
+      </div>
+      <div className="row justify-content-center">
+      <div className="form-group mb-4 col-md-4">
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          placeholder="password"
+          name="password"
+          id="password"
+          className="form-control"
+          onChange={handleChange}
+          value={formData.password}
+        />
+      </div>
+      </div>
+      <center><button type="submit" className="btn btn-primary mb-4">Submit</button></center>
+ 
+       <center>
+      <p className="forgot-password text-right">
+        Haven't Registered? <Link to="/signup">Sign up here</Link>
+      </p>
+      </center>
+    </form>
+  </div>
+  </div>
+     
   );
 }
 
