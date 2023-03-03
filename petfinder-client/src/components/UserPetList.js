e2ff79c377a19f8ce6075c5ddda5f93fc42317c5
matomugo60 <matomugo60@gmail.com>
2023-03-03 10:36:48 +0300

    Updated userpetlist

--- petfinder-client/src/components/UserPetList.js
+++ petfinder-client/src/components/UserPetList.js
@@ -1,87 +1,110 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const UserPetList = () => {
-  const [petList, setPetList] = useState([]);
+const [petList, setPetList] = useState([]);
 
-  const [newPet, setNewPet] = useState({ id: "", name: "", breed: "", owner: "" });
+const [newPet, setNewPet] = useState({ id: "", name: "", breed: "", owner: "" });
 
-  const handleInputChange = (event) => {
-    const { name, value } = event.target;
-    setNewPet({ ...newPet, [name]: value });
-  };
+const handleInputChange = (event) => {
+const { name, value } = event.target;
+setNewPet({ ...newPet, [name]: value });
+};
 
-  const handleAddPet = (event) => {
-    event.preventDefault();
-    setPetList([...petList, newPet]);
-    setNewPet({ id: "", name: "", breed: "", owner: "" });
-  };
+const handleAddPet = async (event) => {
+event.preventDefault();
+try {
+const response = await axios.post("https://petfinder-server.onrender.com/pets", newPet);
+setPetList([...petList, response.data]);
+setNewPet({ id: "", name: "", breed: "", owner: "" });
+} catch (error) {
+console.error(error);
+}
+};
 
-  const handleDeletePet = (petId) => {
-    const updatedPetList = petList.filter((pet) => pet.id !== petId);
-    setPetList(updatedPetList);
-  };
+const handleDeletePet = async (petId) => {
+try {
+await axios.delete(`https://petfinder-server.onrender.com/pets/${petId}`);
+const updatedPetList = petList.filter((pet) => pet.id !== petId);
+setPetList(updatedPetList);
+} catch (error) {
+console.error(error);
+}
+};
 
-  return (
-    <div className="container my-5">
-      <form onSubmit={handleAddPet}>
-        <div className="form-group">
-          <label htmlFor="id">Pet Id:</label>
-          <input
-            type="number"
-            className="form-control"
-            id="id"
-            name="id"
-            value={newPet.id}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="name">Pet Name:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="name"
-            name="name"
-            value={newPet.name}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="breed">Pet Breed:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="breed"
-            name="breed"
-            value={newPet.breed}
-            onChange={handleInputChange}
-          />
-        </div>
-        <div className="form-group">
-          <label htmlFor="owner">Owner Name:</label>
-          <input
-            type="text"
-            className="form-control"
-            id="owner"
-            name="owner"
-            value={newPet.owner}
-            onChange={handleInputChange}
-          />
-        </div>
-        <button type="submit" className="btn btn-primary">
-          Add Pet
-        </button>
-      </form>
-      <table className="table table-striped mt-5">
-        <thead>
-          <tr>
-            <th>Pet Id</th>
-            <th>Pet Name</th>
-            <th>Pet Breed</th>
-            <th>Owner Name</th>
-            <th>Action</th>
-          </tr>
-        </thead>
+useEffect(() => {
+const fetchPets = async () => {
+try {
+const response = await axios.get("https://petfinder-server.onrender.com/pets");
+setPetList(response.data);
+} catch (error) {
+console.error(error);
+}
+};
+fetchPets();
+}, []);
+
+return (
+<div className="container my-5">
+<form onSubmit={handleAddPet}>
+<div className="form-group">
+<label htmlFor="id">Pet Id:</label>
+<input
+         type="number"
+         className="form-control"
+         id="id"
+         name="id"
+         value={newPet.id}
+         onChange={handleInputChange}
+       />
+</div>
+<div className="form-group">
+<label htmlFor="name">Pet Name:</label>
+<input
+         type="text"
+         className="form-control"
+         id="name"
+         name="name"
+         value={newPet.name}
+         onChange={handleInputChange}
+       />
+</div>
+<div className="form-group">
+<label htmlFor="breed">Pet Breed:</label>
+<input
+         type="text"
+         className="form-control"
+         id="breed"
+         name="breed"
+         value={newPet.breed}
+         onChange={handleInputChange}
+       />
+</div>
+<div className="form-group">
+<label htmlFor="owner">Owner Name:</label>
+<input
+         type="text"
+         className="form-control"
+         id="owner"
+         name="owner"
+         value={newPet.owner}
+         onChange={handleInputChange}
+       />
+</div>
+<button type="submit" className="btn btn-primary">
+Add Pet
+</button>
+</form>
+<table className="table table-striped mt-5">
+<thead>
+<tr>
+<th>Pet Id</th>
+<th>Pet Name</th>
+<th>Pet Breed</th>
+<th>Owner Name</th>
+<th>Action</th>
+</tr>
+</thead>
         <tbody>
           {petList.map((pet) => (
             <tr key={pet.id}>
